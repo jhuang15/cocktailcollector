41ed663011bce7bd38ec0bfff9c52476d683aa87
jhuang15 <jchuang1020@gmail.com>
2022-05-11 23:28:53 -0400

    Part 2: Django models

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('cocktails/', views.cocktails_index, name='index'),
+  path('cocktails/<int:cocktail_id>/', views.cocktails_detail, name='detail'),
 ]
